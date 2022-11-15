d79f87ef6ccfb40c5d834986196a80e8aaac713b
Alexander Winkowski <dereference23@outlook.com>
2022-11-15 17:50:17 +0000

    miatoll: Remove Lineage HALs and overlays
    Change-Id: I2f12329994cfd698e6834e64f5d37d1465d12dac

--- device.mk
+++ device.mk
@@ -194,10 +194,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
@@ -268,11 +264,6 @@ PRODUCT_PACKAGES += \
     SM6250SystemUI \
     TelephonyResCommon \
     WifiOverlay
-
-PRODUCT_PACKAGES += \
-    SM6250Aperture \
-    SM6250LineageDialer \
-    SM6250LineageSDK
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -419,10 +410,6 @@ PRODUCT_COPY_FILES += \
 # TimeKeep
 PRODUCT_PACKAGES += \
     TimeKeep
-
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
