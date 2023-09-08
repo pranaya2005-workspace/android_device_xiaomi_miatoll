eb3dbdcb142523a9b4aeb48d1f85c1b4bb542c06
Pranaya Deomani <pranayadeomani@proton.me>
2023-09-08 11:42:23 +0530

    miatoll: Switch to GCC for kernel compilation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,8 +91,10 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
 
-TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
+TARGET_KERNEL_CLANG_COMPILE := false
 TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
+TARGET_KERNEL_LLVM_BINUTILS := true
+TARGET_KERNEL_NEW_GCC_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
